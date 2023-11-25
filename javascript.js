8a57b8ac28c846a155fd1d3ee7c2196d31fec0a1
Wuuny <113912098+MelyssaSemprini@users.noreply.github.com>
2023-11-25 14:37:17 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,107 @@
+//Variáveis 
+var nome="Wei Wuxian", idade=22, clã="YumengJiang";
+let Nome="Lan Wangji", Idade=30, Clã="Gusu Lan";
+let Personagens="Casados";
+let TrindadeMXTX="Svsss,Mdzs,Tgcf";
+var personagens="Shen, Luo, Wei, Wangji, Hua, Xie";
+
+//Function 1° faz uma tarefa, mas não retorna nada.
+  (nome, Nome) => {
+    nome, Nome = Casados;
+  }
+console.log(nome, Nome);
+
+
+function Casadinhos (Personagens){
+    Personagens= Casados;
+}
+
+console.log(Personagens);
+
+console.log(typeof Personagens);
+console.log(Casadinhos);
+
+function Casados (personagens){
+  personagens= Casados
+  };
+  console.log(personagens);
+  Casados("Wei","Wangji");
+  console.log(Casados);
+
+  //fas um calculo/ operação e retorna algo
+
+function Multiplicação (Valor){
+  return Valor*2
+};
+
+let Resultado= Multiplicação(1000);
+console.log(Resultado);
+
+
+//Boolean
+if(Personagens){
+  Casadinhos=true;
+  
+}
+else(Casadinhos)=>{
+  Personagens=false;
+}
+console.log(Personagens);
+
+
+
+//Array
+
+Array=[1 , 2 , 3 , 4 , 5];
+Array2=[6,7,8,9,10];
+Array3=Array.concat(Array2);
+console.log(Array3);
+
+let PersonagensFavs=['Wei', "Hua", "Shen", "Xie"];
+console.log(PersonagensFavs);
+console.log(PersonagensFavs.length);
+
+//Objetos
+
+let Livros = {
+  personagens:'Shen, Luo, Wei, Wangji, Hua, Xie',
+  Personagens:'Casados'
+
+};
+console.log(Livros);
+console.log(typeof Livros);
+
+//Operadores de Atribuição
+
+ValorPcGamer= 5000;
+Dividido=2;
+
+ValorPcGamer*=Dividido;
+
+console.log(ValorPcGamer);
+
+//Operadores de igualdade
+console.log(1===1);
+console.log('1'===1);
+
+
+//Operadores lógicos (&&), retorna true se as duas condições forem verdadeiras
+
+let WuxianCasados= true;
+let WuxianSeAmam= true;
+let JiangChengHetero= false;
+let JiangChengGay= true;
+
+MoDaoZuS = WuxianCasados && WuxianSeAmam;
+Modao = JiangChengHetero && JiangChengGay;
+console.log(MoDaoZuS);
+
+//Operadores Ternários
+
+let Batatas= 100;
+
+let comida = Batatas > 100 ? 'grande' : 'pequena';
+console.log(comida);
+
+
+
